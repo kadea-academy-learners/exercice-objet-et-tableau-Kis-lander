6c61576d2448dac7bda53efcacd9bf69316a4ae6
Kis-lander <abeltenda.gmail.com>
2025-10-29 15:08:21 +0100

    feat : exercise 04 has been done

--- 04-show-student-bulletin.js
+++ 04-show-student-bulletin.js
@@ -14,9 +14,50 @@
 //     - Moyenne >= 10 : "Passable"
 //     - Moyenne < 10  : "À revoir"
 // Dans le cas où l'élève n'a pas de notes, la moyenne doit être considérée comme 0 et le commentaire "À revoir".
+const eleves = [
+	{ nom: "Héritier", notes: [14, 11, 15, 6, 13] },
+	{ nom: "Ruth", notes: [9, 10, 12, 10, 14] },
+	{ nom: "Joseph", notes: [15, 17, 14, 18, 20] },
+	{ nom: "Diane", notes: [] }
+];
+
 function showStudentBulletin(eleves) {
-	
+	return eleves.map(eleves => {
+		// Calcul de la moyenne
+		const notes = eleves.notes || [];
+		const moyenne = notes.length > 0
+		? (notes.reduce((sum, n) => sum + n, 0) / notes.length) : 0;
+
+		// Arrondir à deux décimales
+		const moyenneArrondie = Number(moyenne.toFixed(2));
+
+		// Déterminer le commentaire
+		let commentaire;
+		if (moyenneArrondie >= 16) commentaire = "Excellent";
+		else if (moyenneArrondie >= 14) commentaire = "Très Bien";
+		else if (moyenneArrondie >= 12) commentaire = "Bien";
+		else if (moyenneArrondie >= 10) commentaire = "Passable";
+		else commentaire = "A revoir";
+
+		//Retourner le bulletin de l'élève
+		return {
+			nom: eleves.nom,
+			moyenne: moyenneArrondie,
+			commentaire: commentaire
+		};
+	});
 }
+
+const bulletin = showStudentBulletin(eleves);
+
+console.log("=== Bulletin scolaires ===");
+bulletin.forEach(bulletin => {
+	console.log(`
+		Nom : ${bulletin.nom}
+		Moyenne : ${bulletin.moyenne}
+		Appréciation : ${bulletin.commentaire}
+		--------------------------------`);
+});
 
 module.exports = {
 	showStudentBulletin,
